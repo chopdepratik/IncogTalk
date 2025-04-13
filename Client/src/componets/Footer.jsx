283c9f4cbbd7f4dabdd7d8efcb10afedccf8408a
Pratik Chopde <chopdepratik60@gmail.com>
2025-04-13 09:41:03 +0530

    Update Footer.jsx
    added mail account

--- Client/src/componets/Footer.jsx
+++ Client/src/componets/Footer.jsx
@@ -5,6 +5,7 @@ function Footer() {
   return (
     <footer className="footer">
       <p className="glow-text">Built with ❤️ by <span>Pratik</span></p>
+      <p>chopdepratik60@gmail.com</p>
     </footer>
   );
 }
